588b77695c977da1c87c8ab6f487a2d28c2da543
AnhPhanNguyenLangara <131826328+AnhPhanNguyenLangara@users.noreply.github.com>
2025-02-28 14:39:55 -0800

    Fix word spacing in HTML

--- src/components/HappyBanner/HappyBanner.js
+++ src/components/HappyBanner/HappyBanner.js
@@ -4,8 +4,8 @@ function HappyBanner({howMany}) {
   return (
     <div className="happy banner">
       <p>
-        <strong>Congratulations!</strong> Got it in
-        <strong>{howMany} guesses</strong>.
+        <strong>Congratulations!</strong> Got it in{' '}
+        <strong>{howMany} guess{howMany>1 && 'es'}</strong>.
       </p>
     </div>
   );
